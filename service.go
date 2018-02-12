87b95f671c156c1897ef0d84537e2c7e6abde1e3
Fumihiro Itoh <fmhrit@gmail.com>
2018-02-13 00:17:21 +0900

    add GC stats

--- service.go
+++ service.go
@@ -2,16 +2,24 @@ package statservice
 
 import (
 	"context"
-	"github.com/golang/protobuf/ptypes/any"
 	"runtime"
 	"sync"
 	"time"
+
+	"github.com/golang/protobuf/ptypes/any"
 )
 
 type Service struct {
 	mutex        *sync.Mutex
 	internalFunc func() *any.Any
 }
+
+var (
+	lastSampleTime time.Time
+	lastPauseNs    uint64  = 0
+	lastNumGc      uint32  = 0
+	nsInMs         float64 = float64(time.Millisecond)
+)
 
 func New(internalFunc func() *any.Any) *Service {
 	return &Service{mutex: &sync.Mutex{}, internalFunc: internalFunc}
@@ -34,12 +42,43 @@ func (s *Service) Get(ctx context.Context, req *GetRequest) (*GetResponse, error
 }
 
 func getRuntimeStat() *RuntimeStat {
+	now := time.Now()
 	var mem runtime.MemStats
 	runtime.ReadMemStats(&mem)
 
-	t, _ := time.Now().MarshalText()
+	var gcPausePerSecond float64
+
+	if lastPauseNs > 0 {
+		pauseSinceLastSample := mem.PauseTotalNs - lastPauseNs
+		gcPausePerSecond = float64(pauseSinceLastSample) / nsInMs
+	}
+
+	lastPauseNs = mem.PauseTotalNs
+	countGc := int(mem.NumGC - lastNumGc)
+
+	var gcPerSecond float64
+	if lastNumGc > 0 {
+		diff := float64(countGc)
+		diffTime := now.Sub(lastSampleTime).Seconds()
+		gcPerSecond = diff / diffTime
+	}
+
+	if countGc > 256 {
+		countGc = 256
+	}
+
+	gcPause := make([]float64, countGc)
+	for i := 0; i < countGc; i++ {
+		idx := int((mem.NumGC-uint32(i))+255) % 256
+		pause := float64(mem.PauseNs[idx])
+		gcPause[i] = pause / nsInMs
+	}
+
+	lastNumGc = mem.NumGC
+	lastSampleTime = time.Now()
+
 	return &RuntimeStat{
-		Time:             string(t),
+		Time:             now.Format(time.RFC3339Nano),
 		GoroutineNum:     int32(runtime.NumGoroutine()),
 		CgoCallNum:       int32(runtime.NumCgoCall()),
 		MemoryAlloc:      mem.Alloc,
@@ -58,5 +97,8 @@ func getRuntimeStat() *RuntimeStat {
 		GcNext:           mem.NextGC,
 		GcLast:           mem.LastGC,
 		GcNum:            mem.NumGC,
+		GcPause:          gcPause,
+		GcPerSecond:      gcPerSecond,
+		GcPausePerSecond: gcPausePerSecond,
 	}
 }
